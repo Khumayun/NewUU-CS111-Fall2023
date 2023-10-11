f6e7f7420c5c8b3e5a699b852a0aa4e529cada5f
Khumayun <k.aminaddinov@gmail.com>
2023-10-11 18:46:48 +0500

    Updated structure of the project

--- src/task-3.cpp
+++ src/task-3.cpp
@@ -3,10 +3,3 @@
  * Date:
  * Name:
  */
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- src/task-5.cpp
+++ src/task-5.cpp
@@ -4,9 +4,3 @@
  * Name:
  */
 
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
